58c475bc8ff23e695e251cebc60e20cf65061b60
Sakai-do <125484198+Sakai-do@users.noreply.github.com>
2023-05-02 23:37:11 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,9 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	if (!head)
+		return;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+	free_listint(head->next);
+	free(head);
 }
