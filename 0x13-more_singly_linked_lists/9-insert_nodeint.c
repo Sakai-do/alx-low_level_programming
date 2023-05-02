62b8af921e7ece35ade04f13bdb4980185dc8e07
Sakai-do <125484198+Sakai-do@users.noreply.github.com>
2023-05-02 23:58:48 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	unsigned int i = 0;
+
 	while (i < idx - 1 && current_node)
 	{
 		current_node = current_node->next;
