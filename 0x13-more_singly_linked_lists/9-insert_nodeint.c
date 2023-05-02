54efa67080963a8638602d18ef9f035d7addb09a
Sakai-do <125484198+Sakai-do@users.noreply.github.com>
2023-05-03 00:03:06 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,10 +1,18 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *current_node = *head;
-
-	if (!head)
-		return (NULL);
+	unsigned int i = 0;
+	listint_t *new_node, *current_node = *head, *prev_node = NULL;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
@@ -13,28 +21,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (idx == 0)
+	while (i < idx && current_node)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-
-	unsigned int i = 0;
-
-	while (i < idx - 1 && current_node)
-	{
+		prev_node = current_node;
 		current_node = current_node->next;
 		i++;
 	}
 
-	if (i == idx - 1 && current_node)
+	if (i == idx)
 	{
-		new_node->next = current_node->next;
-		current_node->next = new_node;
+		new_node->next = current_node;
+		if (prev_node)
+			prev_node->next = new_node;
+		else
+			*head = new_node;
+
 		return (new_node);
 	}
-
-	free(new_node);
-	return (NULL);
+	else
+	{
+		free(new_node);
+		return (NULL);
+	}
 }
